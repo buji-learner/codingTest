d3959cbb97e8a1fe9b74b8abff5296bcfd9cc1ea
buji-learner <buji.learning@gmail.com>
2024-07-09 16:37:37 +0100

    첫번째 input split 더하기

--- 240709_Elice_challenge2.py
+++ 240709_Elice_challenge2.py
@@ -30,7 +30,7 @@ sorted >>> {1 1 6 7 8}
 # 첫번째 input: 
 # 배열 크기(N), 일하는 횟수(tries)
 print('Input for length of array(N) and number of tries(tries)')
-input1 = input()
+input1 = input().split(' ')    # input1 = input()
 N, tries = int(input1[0]), int(input1[-1])
 
 # 두번째 input:
@@ -44,14 +44,14 @@ ori_tuple = tuple(ori_list)  #
 
 # input 일하기
 for i in range(tries):
-    print(f'input try{i} numbers :start end k')
+    print(f'input try{i+1} numbers :start end k')
     input_try = input()
     try_list = [int(i) for i in input_try.split(' ')]
 
     new_list = list(ori_list[try_list[0]-1: try_list[1]])
     # print(f'new_list is {new_list}')
     sorted_list = sorted(new_list)
-    print(f'sorted_list is {sorted_list}')
+    # print(f'sorted_list is {sorted_list}')
 
     result = sorted_list[try_list[2]-1]
-    print(f'result is {result}')
+    print(result)
