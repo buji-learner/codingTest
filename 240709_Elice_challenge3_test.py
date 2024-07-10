d6f55cd73445e6cca14434b31d13418869385d61
buji-learner <buji.learning@gmail.com>
2024-07-10 14:11:54 +0100

    func extract_bracket

--- 240709_Elice_challenge3_test.py
+++ 240709_Elice_challenge3_test.py
@@ -24,6 +24,7 @@ output 26
 # 문자열 유효한지 확인
 # def is_VPS:
 
+# K(Q) >>> Q를 K번 반복하는 문자열로 반환
 def K_repeat(S):
     # S = K(Q)
     # K는 한 자리 수, Q를 K번 반복
@@ -33,10 +34,42 @@ def K_repeat(S):
     # return K * len(S_Q)
     return S_Q * K
 
+# 가장 큰  단위의 () 추출
+# 반환값은 첫번째 ( 인덱스, 마지막 ) 인덱스, 숫자로 표현된 input_S 
+def extract_bracket(S):
+    length = len(S)
+
+    list_S = []
+    idx_open = []
+    idx_close = []
+    for i in range(length):
+        if S[i] == '(': 
+            list_S.append(1)
+            idx_open.append(i)
+        elif S[i] == ')': 
+            list_S.append(-1)
+            idx_close.append(i)
+        else : list_S.append(0)
+
+        if sum(list_S) < 0:
+            print(f"{S} is not VPS.")
+            break
+    if sum(list_S) != 0:
+        print(f"{S} is not VPS.")
+
+    print(f'list_S is {list_S}')
+    print(f'location of open/close bracket {idx_open}, {idx_close}')
+    return idx_open, idx_close, list_S
+S = '3(82)' 
+extract_bracket(S)
+
+####------------------
+
 S = '3(82)'
-S = '34)'
+# S = '34)'
 # print(K_repeat(S))
 
+""" 함수 extract_bracket으로 필요없어짐
 # '(' 만나면 +1, ')' 만나면 -1
 stack = 0
 
@@ -48,4 +81,5 @@ for i in range(length):
     if stack < 0:
         print('not VPS')
         break
+"""
     
