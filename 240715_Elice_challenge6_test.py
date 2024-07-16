c42c3e6178675e25d30410182a2ffc5586e6dfca
buji-learner <buji.learning@gmail.com>
2024-07-16 14:23:13 +0100

    don't forget to use DP

--- 240715_Elice_challenge6_test.py
+++ 240715_Elice_challenge6_test.py
@@ -24,23 +24,31 @@ def find_unconnected(dict):
         # print(f'i is {i}')
         for j in range(i, length):
             # print(f'j is {j}')
-            if dict[i][j] == 0:
+            if dict[i][j] == 1:
                 result.append([i, j+1])
                 # print(result)
     
     return result
 
+def select_connected(N):
+    graph = {i: [] for i in range(1, N+1)}
+    for i in range(i, N):
+        
+
+
 # 1, 2
 def make_connected(graph, i, j):
     graph[i][j-1] = 0
     graph[j][i-1] = 0
-    print(f'Trial 1 graph is {graph}')
+    # print(f'Trial 1 graph is {graph}')
+    return graph
 
 # find_unconnected({1: [0, 1, 0, 1], 2:[1, 0, 0, 0], 3:[0, 0, 0, 0], 4:[1, 0, 0, 0]})
 
 
 #----------------------------
 
+# 연결된 것 0, 연결 안된 것 1
 graph = {i: [0 if i-1==j else 1 for j in range(N)] for i in range(1, N+1)}
 print(f'Initial graph is {graph}')
 # ---- {1: [0, 0, 0, 0, 0], 2: [], 3: [], 4: [], 5: []}
@@ -50,6 +58,7 @@ print(f'Initial graph is {graph}')
 # graph[1+1][1-1] = 1
 # print(f'Trial 1 graph is {graph}')
 # >>>
-make_connected(graph, 1, 4)
+graph = make_connected(graph, 1, 4)
+
 
 # for i in range(len(orders)):    
