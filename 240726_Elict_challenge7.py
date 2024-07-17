48cd06947fb8ac705ade61369326149bf6ff619d
buji-learner <buji.learning@gmail.com>
2024-07-17 09:11:43 +0100

    240717 start

--- 240726_Elict_challenge7.py
+++ 240726_Elict_challenge7.py
@@ -1,13 +1,16 @@
 # list = [0, 1]
 # K = 4
-def meetK(list, K):
-    for i in range(10):
-        # print(f'i is {i}')
-        if len(list) == K:
-            break
-        if i not in list:
-            list.append(i)
-    return list
+# def meetK(list, K):
+#     if len(list) > K:
+#         return [i for i in range(K)]
+
+    # for i in range(10):
+    #     # print(f'i is {i}')
+    #     if len(list) == K:
+    #         break
+    #     if i not in list:
+    #         list.append(i)
+    # return list
 
 
 def smallestNum1(list):
@@ -52,16 +55,17 @@ def smallestNum0(list):
 N, K = input().split(' ')
 # print(N, K)
 
-set_N = [int(N[i]) for i in range(len(N))]
-set_N = list(set(set_N))
+list_N = [int(N[i]) for i in range(len(N))]
+sorted_list = sorted(list_N)햐
 # print(set_N)
 int_N, K = int(N), int(K)
 
-numbers = meetK(set_N, K)
+numbers = [i for i in range(K)]
 # print(f'numbers {numbers}')
 # print(f'smallest1 {smallestNum1(numbers)}')
 
 if int_N > smallestNum1(numbers):
-    numbers.remove(1)
-    print(int_N + smallestNum0(numbers))
+    # numbers.remove(sorted_list[1])
+    # print(int_N + smallestNum0(numbers))
+    print(pow(10, (len(N))) + smallestNum0(numbers[2:]))
 else: print(smallestNum1(numbers))
